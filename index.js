057090ef1734741a5b4323edda10a2292dc3d6fe
Andrei Popa <andypopa@gmail.com>
2021-12-10 17:58:29 +0200

    feat: update

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
 
 let d = new Date();
-let targetYear = 2010;
+let targetYear = 2019;
 
 const oneHourInMs = 60 * 60 * 1000;
+const oneDayInMs = 60 * 60 * 1000 * 24;
 
 let i = 0;
 
 while (targetYear < d.getFullYear()) {
-    d = new Date(d.getTime() - oneHourInMs);
-    fs.writeFile(`./data/${d.toISOString()}`, '', () => {});
+    d = new Date(d.getTime() - oneDayInMs);
+    fs.writeFile(`./data/${d.toISOString().replace(/:/g, '_')}`, '', () => {});
     i++;
 }
 
